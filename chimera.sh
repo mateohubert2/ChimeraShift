d2ea42e844a1ff0e19084229b2352b371d3c06f5
mateohubert2 <mathub2003@gmail.com>
2025-04-20 20:26:16 +0200

    ADD - Diiférents choix au lancement

--- chimera.sh
+++ chimera.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 IMAGE="chimeralogo.png"
-
 if [ ! -f "$IMAGE" ]; then
-    echo "Erreur : Le fichier '$IMAGE' n'existe pas."
+    echo "Error: File '$IMAGE' does not exist."
     exit 1
 fi
 
 if ! command -v chafa &> /dev/null; then
-    echo "Erreur : chafa n'est pas installé. Installez-le (ex: sudo apt-get install chafa)."
+    echo "Error: chafa is not installed. Install it (e.g: sudo apt-get install chafa)."
     exit 1
 fi
 
@@ -16,6 +15,68 @@ DIMENSIONS="60x30"
 chafa --size=$DIMENSIONS "$IMAGE"
 
 echo -e "\n"
-echo -e "Utilisez ChimeraShift uniquement sur des systèmes qui vous appartiennent\nou pour lesquels vous disposez d'une autorisation explicite."
+echo -e "Use ChimeraShift only on systems you own or\nfor which you have explicit authorization."
 echo -e "\n"
-echo "Auteur: Matéo HUBERT -> kalilinuxesirem@gmail.com"
+echo "Author: Matéo HUBERT -> kalilinuxesirem@gmail.com"
+echo -e "\n"
+
+options=("Perform a pentest" "See previous pentests" "Quit")
+selected=0
+menu_lines=${#options[@]}
+menu_title="=== Select an option ==="
+
+print_menu() {
+  echo "$menu_title"
+  for i in "${!options[@]}"; do
+    if [ "$i" -eq "$selected" ]; then
+      echo -e "[\e[32mx\e[0m] ${options[$i]}"
+    else
+      echo -e "[ ] ${options[$i]}"
+    fi
+  done
+}
+
+
+print_menu
+
+while true; do
+  IFS= read -rsn1 key
+  if [[ $key == $'\x1b' ]]; then
+    read -rsn1 -t 0.1 k1
+    read -rsn1 -t 0.1 k2
+    key+=$k1$k2
+  fi
+
+  case "$key" in
+    $'\x1b[A')
+      ((selected--))
+      ((selected < 0)) && selected=$((${#options[@]} - 1))
+      ;;
+    $'\x1b[B')
+      ((selected++))
+      ((selected >= ${#options[@]})) && selected=0
+      ;;
+    "")
+      break
+      ;;
+  esac
+
+  tput cuu $((menu_lines + 1))
+  tput el
+  print_menu
+done
+
+echo ""
+echo "------------------------------"
+
+case "${options[$selected]}" in
+  "Perform a pentest")
+    echo "→ Pentest module launched..."
+    ;;
+  "See previous pentests")
+    echo "→ Loading previous reports..."
+    ;;
+  "Quit")
+    echo "→ See you soon!"
+    ;;
+esac
